c275491735e77c3d7f2e788de2cdef5b59b00e81
Andre L Abrantes <contact@andreabrantes.com>
2015-04-28 13:46:14 +0100

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by André Abrantes on 28/04/2015.
+//  Copyright (c) 2015 André Abrantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
